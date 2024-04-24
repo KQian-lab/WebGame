f451decf39d557826cfdc46926ff3a7a9f2e1098
Kevin <keqi8689@colorado.edu>
2024-04-24 14:35:47 -0700

    changed index.html

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
 
-// Send the user to index.html if a route is not recognized
+app.use(express.static('dist'));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
